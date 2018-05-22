df3b40bed0f2543b7ee415ca38cd53d3cb8b14a8
David R <davidrodr95@gmail.com>
2018-05-22 11:42:01 -0500

    Cambio path de images

--- src/js/memorama.js
+++ src/js/memorama.js
@@ -1,20 +1,20 @@
 $(document).on('ready',function(event){
-    var listaImagenes = [".../image/memorama/img01.png",
-                         ".../images/memorama/img02.png",
-                         ".../images/memorama/img03.png",
-                         ".../images/memorama/img04.png",
-                         ".../images/memorama/img05.png",
-                         ".../images/memorama/img06.png",
-                         ".../images/memorama/img07.png",
-                         ".../images/memorama/img08.png",
-                         ".../images/memorama/img01.png",
-                         ".../images/memorama/img02.png",
-                         ".../images/memorama/img03.png",
-                         ".../images/memorama/img04.png",
-                         ".../images/memorama/img05.png",
-                         ".../images/memorama/img06.png",
-                         ".../images/memorama/img07.png",
-                         ".../images/memorama/img08.png"];
+    var listaImagenes = ["../image/memorama/img01.png",
+                         "../images/memorama/img02.png",
+                         "../images/memorama/img03.png",
+                         "../images/memorama/img04.png",
+                         "../images/memorama/img05.png",
+                         "../images/memorama/img06.png",
+                         "../images/memorama/img07.png",
+                         "../images/memorama/img08.png",
+                         "../images/memorama/img01.png",
+                         "../images/memorama/img02.png",
+                         "../images/memorama/img03.png",
+                         "../images/memorama/img04.png",
+                         "../images/memorama/img05.png",
+                         "../images/memorama/img06.png",
+                         "../images/memorama/img07.png",
+                         "../images/memorama/img08.png"];
     var listaRandom;
     var $button = $('button');
     var str = "img";
